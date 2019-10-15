cc19fd23eb888c6e1654c91ee9e389d148ed5d33
mi6ock <miy606@gmail.com>
2019-10-15 15:59:00 +0900

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -39,31 +39,6 @@ class _ChartExampleState extends State<ChartExample> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    final List<String> week = ["Sun", "Mon", "Thu", "Wed", "Thu", "Fri", "Sat"];
-    final List<String> tooltips = [
-      "Good",
-      "Bad",
-      "So So",
-      "A little nice",
-      "Pretty good",
-      "Very good",
-      "Super nice"
-    ];
-
-    Random rnd = Random();
-    for (int i = 0; i < 7; i++) {
-      barDataList.add(
-        BarData(
-          value: rnd.nextDouble() * 10,
-          maxValue: 10,
-          barBackColor: Colors.blue[100],
-          barColor: Colors.blue,
-          title: week[i],
-          toolTip: tooltips[i],
-        ),
-      );
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Chart Example"),
